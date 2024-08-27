38fc8373ba28503864a8f621ce581d213c584f15
KosasihProgramming <growthhuman7@gmail.com>
2024-08-27 08:20:25 +0700

    sjdjkcdkjvndj

--- src/controllers/webHook.js
+++ src/controllers/webHook.js
@@ -33,7 +33,10 @@ exports.handleIncomingMessage = (req, res) => {
 
   const dataReceive = incomingData.data;
 
-  if (dataReceive.is_from_me == false && dataReceive.name !== "status") {
+  if (
+    (dataReceive.is_from_me == false && dataReceive.name !== "status") ||
+    dataReceive.message_body !== ""
+  ) {
     const text = `<b>Chat Dari Pasien </b>\n<b>No. WA. Pasien :</b> ${dataReceive.name} \n<b>Pesan</b> : ${dataReceive.message_body}`;
 
     sendMessage(text)
